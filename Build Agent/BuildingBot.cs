c565a6d16e3fb8add70fe57bbe91b277b28638f3
Cilledyr <cilledyr@gmail.com>
2021-02-02 11:07:32 +0100

    Initial commit

---
+++ Build Agent/BuildingBot.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Build_Agent
+{
+    public class BuildingBot
+    {
+        private string Filepath;
+        private int Delay;
+        private Boolean building = true;
+        private Task t;
+        public BuildingBot(string filePathName, int delay)
+        {
+            Filepath = filePathName;
+            Delay = delay;
+            t = new Task(delegate { loopBuild(); });
+            t.Start();
+        }
+        public void doBuild()
+        {
+            var process = new Process
+            {
+                StartInfo =
+                {
+                    FileName = "dotnet",
+                    Arguments = "build",
+                    WorkingDirectory = Filepath
+                }
+            };
+
+            process.Start();
+            process.WaitForExit();
+            Console.Out.WriteLine("I finished building");
+        }
+
+        public void loopBuild()
+        {
+            while(building)
+            {
+                doBuild();
+                Thread.Sleep(Delay * 1000);
+            }
+        }
+
+        public void setBuilding(Boolean isbuildingOn)
+        {
+            building = isbuildingOn;
+        }
+    }
+}
---
+++ Build Agent/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace Build_Agent
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var bot = new BuildingBot(@"D:\C# shared\Build Agent\Build Agent\", 10);
+            var bot1 = new BuildingBot(@"D:\C# shared\Build Agent\Build Agent\", 3);
+            Console.ReadKey();
+        }
+
+        
+    }
+}
